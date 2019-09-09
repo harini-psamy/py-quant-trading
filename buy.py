c6f088be5365b1f66a52d6be9cd7ff8d917b076a
harini-psamy <47703492+harini-psamy@users.noreply.github.com>
2019-09-09 14:54:32 -0400

    Update buy.py

--- buy.py
+++ buy.py
@@ -1,1 +1,21 @@
+from pylivetrader.api import order_target, record, symbol
+import alpaca_trade_api as tradeapi
+
+api = tradeapi.REST('PKP5E50T3Z70C9IZMTXO', 'VQfcGAQjji3U4kiSh0IxYTgfpAUs4i/YDO1Ik4ZV', api_version='v2') # or use ENV Vars shown below
+account = api.get_account()
+api.list_positions()
+
+def initialize(context):
+    
+    context.sym = symbol('MSFT')
+    context.account_bal = 10000
+    context.units = 10
+    
+def handle_data(context, data):
+    stock_data = data.history(context.asset, 'price', bar_count=100, frequency="1s")
+       
+    order_target(context.asset, units)
+    
+    record(MSFT=data.current(context.asset, 'price'), stock_data)
+
 
