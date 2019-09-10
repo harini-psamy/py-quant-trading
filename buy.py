65a56df84aebb87865298c3c75f5c7c83269b1a1
harini-psamy <47703492+harini-psamy@users.noreply.github.com>
2019-09-09 21:14:51 -0400

    Update buy.py
    Done: Execute the `buy` trade for the requested number of units and plot the value of the current account balance w.r.t time. 
    Incomplete: The plot should auto-update at least every second showing the real-time value of the balance until the script is terminated.

--- buy.py
+++ buy.py
@@ -1,21 +1,25 @@
 from pylivetrader.api import order_target, record, symbol
 import alpaca_trade_api as tradeapi
+import sys
+import matplotlib.pyplot as plt
+#hndl = tradeapi.REST()
+api = tradeapi.REST(key_id = 'PKK3QYZDA5NDVLEV81S1', secret_key='LU8PBU9InDaoK1YD0MAlEHtXhTaQvPZVInr6Fifp',base_url='https://paper-api.alpaca.markets')#, api_version='v2') # or use ENV Vars shown below
 
-api = tradeapi.REST('PKP5E50T3Z70C9IZMTXO', 'VQfcGAQjji3U4kiSh0IxYTgfpAUs4i/YDO1Ik4ZV', api_version='v2') # or use ENV Vars shown below
 account = api.get_account()
-api.list_positions()
+print("status: ",account.status)
+print("positions:",api.list_positions())
 
-def initialize(context):
-    
-    context.sym = symbol(sym)
-    context.account_bal = account-bal
-    context.units = units
-    
-def handle_data(context, data):
-    stock_data = data.history(context.asset, 'price', bar_count=100, frequency="1s")
-       
-    order_target(context.asset, units)
-    
-    record(MSFT=data.current(context.asset, 'price'), stock_data)
+api.cancel_all_orders()
+Balance, symbol, units = sys.argv[1],sys.argv[2],sys.argv[3]
+
+Balance = Balance.replace('--account_bal=','')
+symbol = symbol.replace('--sym=','')
+units = units.replace('--units=','')
+
+api.submit_order(symbol = symbol, qty = units, side = 'buy', type = 'market', time_in_force ='gtc', limit_price=None, stop_price=None, client_order_id=None)
 
 
+dates = matplotlib.dates.date2num(list_of_datetimes)
+plt.plot(dates,account.portfolio_value)
+#to add timestamp frequency
+plt.show()
